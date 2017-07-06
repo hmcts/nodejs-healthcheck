0a3f0ebab1c9fa755229bb9a3c14d75008be8b70
Michael Allen <michael.allen@hmcts.net>
2017-07-06 09:53:10 +0100

    Hook new commit parameter in to buildInfo block

--- healthcheck/routes.js
+++ healthcheck/routes.js
@@ -1,37 +1,44 @@
 'use strict'
 const checks = require('./checks'),
-      outputs = require('./outputs');
+      outputs = require('./outputs'),
+      versionFile = require('./versionFile');
 
 function getBuildInfo(extra) {
-  let buildInfo = {
-    environment: process.env.PACKAGES_ENVIRONMENT || "unknown",
-    project: process.env.PACKAGES_PROJECT || "unknown",
-    name: process.env.PACKAGES_NAME || "unknown",
-    version: process.env.PACKAGES_VERSION || "unknown",
-  };
-  if (extra) {
-    buildInfo.extra = extra;
-  }
-  return buildInfo;
+  return Promise.all([
+    versionFile.commit()
+  ]).then(([commit]) => {
+    let buildInfo = {
+      environment: process.env.PACKAGES_ENVIRONMENT || "unknown",
+      project: process.env.PACKAGES_PROJECT || "unknown",
+      name: process.env.PACKAGES_NAME || "unknown",
+      version: process.env.PACKAGES_VERSION || "unknown",
+      commit,
+    };
+    if (extra) {
+      return Object.assign(buildInfo, { extra });
+    } else {
+      return buildInfo;
+    }
+  });
 }
 
 function configure(config) {
   const check = new checks.CompositeCheck(config.checks);
 
   return (req, res) => {
-    check.call().then(results => {
-      const allOk = Object.values(results)
-                          .every(result => result.status === outputs.UP);
-      const output = Object.assign(
-        outputs.status(allOk),
-        results,
-        {
-          buildInfo: getBuildInfo(config.buildInfo)
-        }
-      );
-      const status = allOk ? 200 : 500;
-      res.status(status).json(output);
-    });
+    return Promise
+      .all([check.call(), getBuildInfo(config.buildInfo)])
+      .then(([results, buildInfo]) => {
+        const allOk = Object.values(results)
+                            .every(result => result.status === outputs.UP);
+        const output = Object.assign(
+          outputs.status(allOk),
+          results,
+          { buildInfo }
+        );
+        const status = allOk ? 200 : 500;
+        res.status(status).json(output);
+      });
   }
 }
 
--- test/unit/routesTest.js
+++ test/unit/routesTest.js
@@ -5,6 +5,7 @@ const {expect, sinon} = require('../chai-sinon'),
       routes = require('../../healthcheck/routes'),
       checks = require('../../healthcheck/checks'),
       outputs = require('../../healthcheck/outputs'),
+      versionFile = require('../../healthcheck/versionFile'),
       nock = require('nock');
 
 
@@ -16,6 +17,8 @@ describe('Routes', () => {
   ];
 
   beforeEach(() => {
+    sinon.stub(versionFile, 'commit');
+    versionFile.commit.resolves('abc1234');
     envKeys.forEach(key => {
       originalEnv[key] = process.env[key];
       process.env[key] = "test " + key;
@@ -23,6 +26,7 @@ describe('Routes', () => {
   });
 
   afterEach(() => {
+    versionFile.commit.restore();
     envKeys.forEach(key => {
       if (typeof originalEnv[key] === "undefined") {
         delete process.env[key];
@@ -36,25 +40,25 @@ describe('Routes', () => {
   describe('getBuildInfo', () => {
 
     it('should add build info from environment', () => {
-      expect(routes.getBuildInfo()).to.eql({
+      return expect(routes.getBuildInfo()).to.eventually.eql({
         environment: "test PACKAGES_ENVIRONMENT",
         project: "test PACKAGES_PROJECT",
         name: "test PACKAGES_NAME",
         version: "test PACKAGES_VERSION",
+        commit: 'abc1234'
       });
     });
 
     it('should include extra build info', () => {
-      expect(routes.getBuildInfo({"foo": "bar"}).extra).to.eql({
-        "foo": "bar"
-      });
+      const extra = routes.getBuildInfo({ foo: "bar" }).then(_ => _.extra);
+      return expect(extra).to.eventually.eql({ foo: "bar" });
     });
 
   });
 
   describe('configure', () => {
     let makeCheck = isOk => checks.RawCheck.create(() => isOk ? outputs.up() : outputs.down());
-    let makeReqRes = (done, expectedStatus, expectedJson) => {
+    let makeReqRes = (expectedStatus, expectedJson) => {
       let req = sinon.spy(),
           res = {};
       res.status = status => {
@@ -63,20 +67,19 @@ describe('Routes', () => {
       };
       res.json = json => {
         expect(json).to.eql(expectedJson);
-        done();
         return res;
       };
       return [req, res];
     };
 
-    it('should return 200 OK if all checks pass', (done) => {
+    it('should return 200 OK if all checks pass', () => {
       let route = routes.configure({
         checks: {
           check1: makeCheck(true),
           check2: makeCheck(true),
         }
       });
-      let [req, res] = makeReqRes(done, 200, {
+      let [req, res] = makeReqRes(200, {
         status: outputs.UP,
         check1: {status: "UP"},
         check2: {status: "UP"},
@@ -85,20 +88,21 @@ describe('Routes', () => {
           project: "test PACKAGES_PROJECT",
           name: "test PACKAGES_NAME",
           version: "test PACKAGES_VERSION",
+          commit: 'abc1234'
         }
       });
 
-      route(req, res);
+      return route(req, res);
     });
 
-    it('should return 500 DOWN if any checks fail', (done) => {
+    it('should return 500 DOWN if any checks fail', () => {
       let route = routes.configure({
         checks: {
           check1: makeCheck(false),
           check2: makeCheck(true),
         }
       });
-      let [req, res] = makeReqRes(done, 500, {
+      let [req, res] = makeReqRes(500, {
         status: "DOWN",
         check1: {status: "DOWN"},
         check2: {status: "UP"},
@@ -107,14 +111,15 @@ describe('Routes', () => {
           project: "test PACKAGES_PROJECT",
           name: "test PACKAGES_NAME",
           version: "test PACKAGES_VERSION",
+          commit: 'abc1234'
         }
       });
 
-      route(req, res);
+      return route(req, res);
 
     });
 
-    it('should return the extra build info', (done) => {
+    it('should return the extra build info', () => {
       let route = routes.configure({
         checks: {
           check1: makeCheck(true)
@@ -123,7 +128,7 @@ describe('Routes', () => {
           foo: "bar"
         }
       });
-      let [req, res] = makeReqRes(done, 200, {
+      let [req, res] = makeReqRes(200, {
         status: "UP",
         check1: {status: "UP"},
         buildInfo: {
@@ -131,13 +136,14 @@ describe('Routes', () => {
           project: "test PACKAGES_PROJECT",
           name: "test PACKAGES_NAME",
           version: "test PACKAGES_VERSION",
+          commit: 'abc1234',
           extra: {
             foo: "bar"
           }
         }
       });
 
-      route(req, res);
+      return route(req, res);
     });
 
   });
