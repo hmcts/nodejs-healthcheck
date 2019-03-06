8612fa601a42983602202156426ec062fc7a4b4e
Karim Lawani <k7lawani@gmail.com>
2019-03-06 16:54:37 +0000

    revert routes.js to the same as in master branch ... previous changes were due to formatting.

--- healthcheck/routes.js
+++ healthcheck/routes.js
@@ -1,7 +1,7 @@
 'use strict'
-const checks = require('./checks')
-const outputs = require('./outputs')
-const versionFile = require('./versionFile')
+const checks = require('./checks'),
+      outputs = require('./outputs'),
+      versionFile = require('./versionFile');
 
 function getBuildInfo(extra) {
   return Promise.all([
@@ -10,43 +10,43 @@ function getBuildInfo(extra) {
     versionFile.date()
   ]).then(([version, commit, date]) => {
     let buildInfo = {
-      environment: process.env.PACKAGES_ENVIRONMENT || process.env.REFORM_ENVIRONMENT || 'unknown',
-      project: process.env.PACKAGES_PROJECT || process.env.REFORM_TEAM || 'unknown',
-      name: process.env.PACKAGES_NAME || process.env.REFORM_SERVICE_NAME || 'unknown',
+      environment: process.env.PACKAGES_ENVIRONMENT || process.env.REFORM_ENVIRONMENT || "unknown",
+      project: process.env.PACKAGES_PROJECT || process.env.REFORM_TEAM || "unknown",
+      name: process.env.PACKAGES_NAME || process.env.REFORM_SERVICE_NAME || "unknown",
       version,
       commit,
       date
-    }
+    };
     if (extra) {
-      return Object.assign(buildInfo, {extra})
+      return Object.assign(buildInfo, { extra });
     } else {
-      return buildInfo
+      return buildInfo;
     }
-  })
+  });
 }
 
 function configure(config) {
-  const check = new checks.CompositeCheck(config.checks)
+  const check = new checks.CompositeCheck(config.checks);
 
   return (req, res) => {
     return Promise
       .all([check.call(req, res), getBuildInfo(config.buildInfo)])
       .then(([results, buildInfo]) => {
         const allOk = Object.values(results)
-          .every(result => result.status === outputs.UP)
+                            .every(result => result.status === outputs.UP);
         const output = Object.assign(
           outputs.status(allOk),
           results,
-          {buildInfo}
-        )
-        const status = allOk ? 200 : 500
-        res.status(status).json(output)
-      })
+          { buildInfo }
+        );
+        const status = allOk ? 200 : 500;
+        res.status(status).json(output);
+      });
   }
 }
 
 
 module.exports = {
-  'getBuildInfo': getBuildInfo,
-  'configure': configure,
-}
+  "getBuildInfo": getBuildInfo,
+  "configure": configure,
+};
