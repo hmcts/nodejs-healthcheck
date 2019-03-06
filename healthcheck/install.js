f7fff5fb572b22c16c697eb234a99c229fb7e39f
Karim Lawani <k7lawani@gmail.com>
2019-03-06 10:49:40 +0000

    remove Console.log

--- healthcheck/install.js
+++ healthcheck/install.js
@@ -4,9 +4,7 @@ const routes = require('./routes')
 const outputs = require('./outputs')
 
 function addTo(app, config) {
-  console.log("adding healthCheck ")
   app.get('/health', routes.configure(config))
-  console.log("adding liveness")
   app.get('/health/liveness', (req, res) => res.status(200).json(outputs.status(outputs.UP)))
 }
 
